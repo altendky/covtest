1071c7f999a9ed51154a48148819a01ebc5cdd11
Kyle Altendorf <sda@fstab.net>
2020-07-30 20:01:07 -0400

    more info

--- go.sh
+++ go.sh
@@ -1,7 +1,16 @@
+set -evx
+
 rm -rf venv
 python3 -m venv venv
 venv/bin/pip install --upgrade pip setuptools wheel
 venv/bin/pip install .
+
+echo
+
+venv/bin/python --version --version
+git show --no-patch HEAD
+venv/bin/pip freeze --all
+venv/bin/coverage debug sys
 
 venv/bin/coverage erase
 rm -rf htmlcov
@@ -9,6 +18,7 @@ venv/bin/coverage run --rcfile .coveragerc -m covtest
 venv/bin/coverage html
 echo ------- root
 venv/bin/coverage report
+echo
 grep -r missing htmlcov/*__main__*
 
 echo
@@ -23,5 +33,6 @@ rm -rf htmlcov
 ../venv/bin/coverage html
 echo ------- empty/
 ../venv/bin/coverage report
+echo
 grep -r missing htmlcov/*__main__*
 cd -
